df91846e7355891d5beecc2fac6734ff6c2d2605
Mehrdad08 <moradi.mehrdad001@gmail.com>
2022-08-20 14:08:01 +0430

    1- Add left_shape_background.dart & right_shape_background.dart
    2- use const constructor methode for input

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
+import 'package:calculate_bmi/widgets/left_shape_background.dart';
+import 'package:calculate_bmi/widgets/right_shape_background.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(Application());
+}
 
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              RightShape(),
+              RightShape(),
+              RightShape(),
+              LeftShape(width: 150,),
+              LeftShape(width: 200),
+              LeftShape(width: 150),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
---
+++ lib/widgets/left_shape_background.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LeftShape extends StatelessWidget {
+  final double? width;
+  const LeftShape({Key? key, this.width}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          width: width,
+          height: 40.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+            color: Colors.green,
+          ),
+        ),
+      ],
+    );
+  }
+}
